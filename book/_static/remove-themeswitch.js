d1dffbf90ebf910c031f6bbbc183357641034ebc
petvat <peter.vatno02@gmail.com>
2025-04-10 12:49:28 +0200

    docs: add comment to theme switch script

--- book/_static/remove-themeswitch.js
+++ book/_static/remove-themeswitch.js
@@ -1,3 +1,6 @@
+/**
+ * Fix to disable theme switching on the page. Removes the the button for switching themes.
+ */
 document.addEventListener("DOMContentLoaded", function () {
     const btn = document.getElementsByClassName("btn btn-sm nav-link pst-navbar-icon theme-switch-button")[0];
     if (btn) {
